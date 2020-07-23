aca2bbf3972134e1f9459ad9e949fb51d4fb1c78
Atul Pandey <atulpan98@gmail.com>
2020-07-23 23:14:45 +0530

    added loader icon

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,13 @@
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector(".loader").style.visibility = "visible";
+  } else {
+    document.querySelector(".loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
+
 $(document).ready(function () {
   // For sticky navigation
   $("nav").removeClass("sticky");
